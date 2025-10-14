e9953f02d36ee5620e49be33ca966760e6a9193a
harshita2012sharma-max <harshita2012sharma@gmail.com>
2025-10-14 11:21:18 +0530

    Add files via upload

---
+++ type casting/viva la vida(type casting).py
@@ -0,0 +1,137 @@
+# data=int(input("enter the number : "))
+# print(type(data))
+# data_1=list(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=int(input("enter the number : "))
+# print(type(data))
+# data_1=tuple(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=int(input("enter the number : "))
+# print(type(data))
+# data_1=complex(data)
+# print(data_1)
+# print(type(data_1))
+      
+                                  #FLOAT
+
+# data=float(input("enter the number : "))
+# print(type(data))
+# data_1=int(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=float(input("enter the number : "))
+# print(type(data))
+# data_1=list(data)
+# print(data_1)
+# print(type(data_1))
+
+                                   #STRING
+
+# data=input("enter the number : ")
+# print(type(data))
+# data_1=int(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=input("enter :")
+# data_1=list(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=input("enter :")
+# data_1=tuple(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=input("enter :")
+# data_1=dict(data)
+# print(data_1)
+# print(type(data_1))
+
+# data=input("enter :")
+# data_1=complex(data)
+# print(data_1)
+# print(type(data_1))
+
+                                    #list
+
+# data=list(input("enter :"))
+# data_1=str(data)
+# print(data_1)               
+# print(type(data_1))
+""" YES """
+
+# data=list(input("enter :"))
+# data_1=tuple(data)
+# print(data_1)
+# print(type(data_1))
+""" YES """
+
+# data=list(input("enter :"))
+# data_1=set(data)
+# print(data_1)
+# print(type(data_1))
+""" YES """
+
+# data=list(input("enter :"))
+# data_1=dict(data)
+# print(data_1)
+# print(type(data_1))
+""" NO """
+
+                      #TUPLE
+
+# data=tuple(input("enter :"))
+# data_1=int(data)
+# print(data_1)
+# print(type(data_1))
+""" SAME AS LIST """
+
+                           # SET
+
+# data=set(input("enter :"))
+# data_1=str(data)
+# print(data_1)
+# print(type(data_1))
+
+                                  # DICTIONARY
+
+# data=dict(input("enter :"))
+# data_1=str(data)
+# print(data_1)
+# print(type(data_1))       
+""" NO """                          
+
+# data=dict(input("enter :"))
+# data_1=set(data)
+# print(data_1)
+# print(type(data_1)) 
+""" NO """
+
+                              # COMPLEX
+
+# data=complex(input("enter :"))
+# print(type(data))
+# data_1=int(data)
+# print(data_1)
+# print(type(data_1))                               
+""" NO """
+
+# data=complex(input("enter :"))
+# print(type(data))
+# data_1=str(data)
+# print(data_1)
+# print(type(data_1))  
+""" YES """
+
+# data=complex(input("enter :"))
+# print(type(data))
+# data_1=list(data)
+# print(data_1)
+# print(type(data_1))  
+""" NO """
